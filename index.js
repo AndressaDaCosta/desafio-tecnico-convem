8b904b7ac54805a78571220edd53097455f8c676
Andressa Da Costa <andressadacostan@gmail.com>
2023-05-13 15:44:11 -0300

    API NodeJS :white_check_mark:

--- index.js
+++ index.js
@@ -5,3 +5,27 @@
 
     3- Integrar com a aplicação em Angular.
 */
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.post('/api/response', (req, res) => {
+	const userResponse = req.body.userResponse.toLowerCase()
+	if (userResponse === 'sim') {
+		res.status(200).send('Successo')
+	} else {
+		res.status(400).send('Erro')
+	}
+})
+
+app.listen(PORT, () => {
+	console.log(`API running on port ${PORT}`)
+})
+
+// JSON: curl -X POST -H "Content-Type: application/json" -d '{"answer":"sim"}' http://localhost:3000/api/answer
