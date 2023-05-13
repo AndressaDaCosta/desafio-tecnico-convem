9f3055c0aac05ddb609e5001e420577efd8e9bb4
Andressa Da Costa <andressadacostan@gmail.com>
2023-05-13 15:29:54 -0300

    packages-config :bulb:

---
+++ index.js
@@ -0,0 +1,7 @@
+/*  1 - Desenvolver uma API em Noje.js do zero?
+    2- Instalação de pacotes: 
+        - Express: para criar um servidor HTTP.
+        - Body-parser: para interpretar o corpo das requisições recebidas pelo servidor.
+
+    3- Integrar com a aplicação em Angular.
+*/
