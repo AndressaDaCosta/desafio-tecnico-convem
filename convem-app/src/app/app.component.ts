0eb65f94e395684a9724676f8dbc4164809e26da
Andressa Da Costa <andressadacostan@gmail.com>
2023-05-15 12:26:16 -0300

    front-end integration :building_construction:

--- convem-app/src/app/app.component.ts
+++ convem-app/src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'convem-app';
+	titulo = 'Você quer se juntar ao time da Convem?';
+	resposta: string = '';
+	mensagem: string = '';
+
+	constructor(private http: HttpClient) {}
+
+	enviarResposta() {
+		this.http
+			.post<any>('http://localhost:3000/api/resposta', {
+				resposta: this.resposta,
+			})
+			.subscribe(
+				(response) => {
+					if (response === 'Sucesso') {
+						this.mensagem =
+							'Você está mais próximo de se juntar ao time!';
+					} else {
+						this.mensagem = 'Erro';
+					}
+				},
+				(error) => {
+					console.log(error);
+				}
+			);
+	}
 }
